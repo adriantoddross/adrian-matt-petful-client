c216b758444407e114e216403539b0c29df7cf18
Adrian <adriantoddross@gmail.com>
2018-04-13 18:09:34 -0400

    change fetch method to DELETE

--- src/reducers/cat.js
+++ src/reducers/cat.js
@@ -1,8 +1,8 @@
-import { FETCH_CAT_SUCCESS, FETCH_CAT_REQUEST, FETCH_CAT_ERROR } from '../actions/index';
+import { FETCH_CAT_SUCCESS, FETCH_CAT_REQUEST, FETCH_CAT_ERROR, DELETE_CAT_ERROR, DELETE_CAT_SUCCESS, DELETE_CAT_REQUEST, deleteCat } from '../actions/index';
 
 const initialState = {
-  animal: null, 
-  error: null, 
+  animal: null,
+  error: null,
   loading: false
 };
 
@@ -15,5 +15,17 @@ export function catReducer(state=initialState, action) {
   }
   if (action.type === FETCH_CAT_SUCCESS) {
     return Object.assign({}, state, { loading: false, error: null, animal: action.animal});
-  } return state;
+  }
+
+  if (action.type === DELETE_CAT_REQUEST) {
+    return Object.assign({}, state, { loading: true, error: null });
+  }
+  if (action.type === DELETE_CAT_ERROR) {
+    return Object.assign({}, state, { loading: false, error: action.error });
+  }
+  if (action.type === DELETE_CAT_SUCCESS) {
+    return Object.assign({}, state, { loading: false, error: null, animal: null});
+  }
+
+  return state;
 }

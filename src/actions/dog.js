ecd7b90607bce1653d889ef752cece9af240c717
Adrian <adriantoddross@gmail.com>
2018-04-13 18:34:22 -0400

    add deleteDog actions

--- src/actions/dog.js
+++ src/actions/dog.js
@@ -23,9 +23,40 @@ export const fetchDog = (animal) => dispatch => {
   .then(res => {
     if (!res.ok) {
       throw new Error('Dog not found');
-    } 
+    }
     return res.json(res);
   })
   .then(animal => {dispatch(fetchDogSuccess(animal))})
   .catch(err => {dispatch(fetchDogError(err))});
+}
+
+
+export const DELETE_DOG_SUCCESS = 'DELETE_DOG_SUCCESS';
+export const DELETE_DOG_REQUEST = 'DELETE_DOG_REQUEST';
+export const DELETE_DOG_ERROR = 'DELETE_DOG_ERROR';
+
+export const deleteDogSuccess = animal => ({
+  type: DELETE_DOG_SUCCESS,
+  animal
+});
+export const deleteDogRequest = () => ({
+  type: DELETE_DOG_REQUEST
+});
+export const deleteDogError = error => ({
+  type: DELETE_DOG_ERROR,
+  error
+});
+
+export const deleteDog = () => dispatch => {
+  dispatch(deleteDogRequest());
+  return fetch(`${API_BASE_URL}/api/cat`, {
+    method: 'DELETE',
+  })
+  .then(res => {
+    console.log(res);
+    // return res.json(res);
+  })
+  .then( () => {dispatch(deleteDogSuccess())})
+  .then(() => {dispatch(fetchDog())})
+  .catch(err => {dispatch(deleteDogError(err))});
 }

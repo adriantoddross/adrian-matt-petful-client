089421cd61744072daeaedba2acecbb7766c0e63
mattNat <mattsanpedro76@gmail.com>
2018-04-13 11:14:30 -0700

    move hard-coded data to Pet.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,47 +1,32 @@
 import React from 'react';
+import Pet from './Pet';
 
 export default function Dashboard(props) {
+  const Fluffy = {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+  
+  const Zeus = {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
 
     return (
       <div className="App">
-      <section>
-        <header>
-          <h3>{props.catToAdopt.name}</h3>
-          <img src={props.catToAdopt.imageURL} alt={props.catToAdopt.imageDescription} className='animal-image'/>
-        </header>
-          <main role='main'>
-              <dl>
-                <dt>Sex</dt>
-                <dd>{props.catToAdopt.sex}</dd>
-                <dt>Age</dt>
-                <dd>{props.catToAdopt.age} years</dd>
-                <dt>Breed</dt>
-                <dd>{props.catToAdopt.breed}</dd>
-                <dt>Story</dt>
-                <dd>{props.catToAdopt.story}</dd>
-              </dl>
-            </main>
-            <button type="submit">Adopt</button>
-      </section>
-      <section>
-        <header>
-          <h3>{props.dogToAdopt.name}</h3>
-          <img src={props.dogToAdopt.imageURL} alt={props.dogToAdopt.imageDescription} className='animal-image'/>
-        </header>
-          <main role='main'>
-              <dl>
-                <dt>Sex</dt>
-                <dd>{props.dogToAdopt.sex}</dd>
-                <dt>Age</dt>
-                <dd>{props.dogToAdopt.age} years</dd>
-                <dt>Breed</dt>
-                <dd>{props.dogToAdopt.breed}</dd>
-                <dt>Story</dt>
-                <dd>{props.dogToAdopt.story}</dd>
-              </dl>
-            </main>
-            <button type="submit">Adopt</button>
-      </section>
+        <Pet animal={Fluffy}/>
+        <Pet animal={Zeus}/>
+        
       </div>
     );
 }

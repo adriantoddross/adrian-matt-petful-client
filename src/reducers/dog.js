dfa6dea848943d922e5ddc4f04126c7b2d64fec8
Adrian <adriantoddross@gmail.com>
2018-04-13 18:36:45 -0400

    import deleteDog actions and add them to reducer

--- src/reducers/dog.js
+++ src/reducers/dog.js
@@ -1,8 +1,8 @@
-import { FETCH_DOG_SUCCESS, FETCH_DOG_REQUEST, FETCH_DOG_ERROR } from '../actions/index';
+import { FETCH_DOG_SUCCESS, FETCH_DOG_REQUEST, FETCH_DOG_ERROR, DELETE_DOG_ERROR, DELETE_DOG_SUCCESS, DELETE_DOG_REQUEST } from '../actions/index';
 
 const initialState = {
-  animal: null, 
-  error: null, 
+  animal: null,
+  error: null,
   loading: false
 };
 
@@ -16,5 +16,17 @@ export function dogReducer(state=initialState, action) {
   if (action.type === FETCH_DOG_SUCCESS) {
     console.log(action.animal);
     return Object.assign({}, state, { loading: false, error: null, animal: action.animal});
-  } return state;
+  }
+
+  if (action.type === DELETE_DOG_REQUEST) {
+    return Object.assign({}, state, { loading: true, error: null });
+  }
+  if (action.type === DELETE_DOG_ERROR) {
+    return Object.assign({}, state, { loading: false, error: action.error });
+  }
+  if (action.type === DELETE_DOG_SUCCESS) {
+    return Object.assign({}, state, { loading: false, error: null, animal: null});
+  }
+
+  return state;
 }

223a56b23cf06c3779e4768496badeab29285a78
Adrian <adriantoddross@gmail.com>
2018-04-13 12:07:51 -0400

    add props for catToAdopt and dogToAdopt

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,78 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 
-export default function Dashboard(props) {
 
-
-  // render() {
-    const catToAdopt = {
-      class: 'one',
-      name: 'Fluffy',
-      url: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-      sex: 'Female',
-      age: '2',
-      breed: 'Bengal',
-      story: 'Thrown on the street.'
-    }
-
-    const dogToAdopt = {
-      class: 'two',
-      name: 'June',
-      url: 'http://img.freepik.com/free-photo/husky-breed-dog-with-tongue-out_1187-1500.jpg?size=338&ext=jpg',
-      sex: 'Female',
-      age: 1,
-      breed: 'Bengal',
-      story: 'Thrown on the street.'
-    }
-
-    // function Display(props) {
-    // //   return (
-      
-    //     <div className={props.class}>
-    //         <header>
-    //           <h3>{props.name}</h3>
-    //           <img src={props.url} alt={props.name} className='animal-image' />
-    //         </header>
-    //         <main role='main'>
-    //           <dl>
-    //             <dt>Sex</dt>
-    //             <dd>{props.sex}</dd>
-    //             <dt>Age</dt>
-    //             <dd>{props.age} years</dd>
-    //             <dt>Breed</dt>
-    //             <dd>{props.breed}</dd>
-    //             <dt>Story</dt>
-    //             <dd>{props.story}</dd>
-    //             <button>Adopt</button>
-    //           </dl>
-    //         </main>
-    //     </div>
-    // //   )
-    // }
+export default function Dashboard(catToAdopt, dogToAdopt) {
 
     return (
       <div className="App">
-        <div className={props.class}>
-            <header>
-              <h3>{props.name}</h3>
-              <img src={props.url} alt={props.name} className='animal-image' />
-            </header>
-            <main role='main'>
+      <section>
+        <header>
+          <h3>{catToAdopt.name}</h3>
+          <img src={catToAdopt.imageURL} alt={catToAdopt.imageDescription} className='animal-image'/>
+        </header>
+          <main role='main'>
               <dl>
                 <dt>Sex</dt>
-                <dd>{props.sex}</dd>
+                <dd>{catToAdopt.sex}</dd>
                 <dt>Age</dt>
-                <dd>{props.age} years</dd>
+                <dd>{catToAdopt.age} years</dd>
                 <dt>Breed</dt>
-                <dd>{props.breed}</dd>
+                <dd>{catToAdopt.breed}</dd>
                 <dt>Story</dt>
-                <dd>{props.story}</dd>
+                <dd>{catToAdopt.story}</dd>
                 <button>Adopt</button>
               </dl>
             </main>
-        </div>
-        
+            <button type="submit">Adopt</button>
+      </section>
+      <section>
+        <header>
+          <h3>{dogToAdopt.name}</h3>
+          <img src={dogToAdopt.imageURL} alt={dogToAdopt.imageDescription} className='animal-image'/>
+        </header>
+          <main role='main'>
+              <dl>
+                <dt>Sex</dt>
+                <dd>{dogToAdopt.sex}</dd>
+                <dt>Age</dt>
+                <dd>{dogToAdopt.age} years</dd>
+                <dt>Breed</dt>
+                <dd>{dogToAdopt.breed}</dd>
+                <dt>Story</dt>
+                <dd>{dogToAdopt.story}</dd>
+                <button>Adopt</button>
+              </dl>
+            </main>
+            <button type="submit">Adopt</button>
+      </section>
       </div>
     );
-  // }
 }

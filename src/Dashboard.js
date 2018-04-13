0234652846f60d1d919a9a7ca9c57830f4991033
mattNat <mattsanpedro76@gmail.com>
2018-04-13 13:47:50 -0700

    tested componentDidMount()

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,44 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pet from './Pet';
 
 import { connect } from 'react-redux';
 import { fetchCat, fetchDog } from './actions/index';
 
-export function Dashboard(props) {
+export class Dashboard extends Component {
+  componentDidMount(){
+    this.props.dispatch(fetchCat());
+  //   this.props.dispatch(fetchDog());
 
-
-  const Fluffy = {
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  }
-  
-  const Zeus = {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
   }
 
-  return (
-    <div className="App">
-      <Pet 
-        animal={Fluffy} 
-        onAdoptPet={() => {console.log('Pet adopted')}} 
-      />
-      <Pet 
-        animal={Zeus} 
-        onAdoptPet={() => {console.log('Pet adopted')}} 
-      />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {/* <h3>{this.props.cat}</h3> */}
+        {/* <Pet 
+          animal={this.props.cat} 
+          onAdoptPet={() => {console.log('Pet adopted')}} 
+        />
+        <Pet 
+          animal={this.props.dog} 
+          onAdoptPet={() => {console.log('Pet adopted')}} 
+        /> */}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({

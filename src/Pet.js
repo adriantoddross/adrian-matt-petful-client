17906da6b152b855ad0744e49e329a50049932d9
mattNat <mattsanpedro76@gmail.com>
2018-04-13 11:10:47 -0700

    add Pet.js

---
+++ src/Pet.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Pet(props) {
+
+  return(
+    <section>
+      <header>
+        <h3>{props.animal.name}</h3>
+        <img src={props.animal.imageURL} alt={props.animal.imageDescription} className='animal-image'/>
+      </header>
+        <main role='main'>
+            <dl>
+              <dt>Sex</dt>
+              <dd>{props.animal.sex}</dd>
+              <dt>Age</dt>
+              <dd>{props.animal.age} years</dd>
+              <dt>Breed</dt>
+              <dd>{props.animal.breed}</dd>
+              <dt>Story</dt>
+              <dd>{props.animal.story}</dd>
+            </dl>
+          </main>
+          <button type="submit">Adopt</button>
+      </section>
+  )
+}
